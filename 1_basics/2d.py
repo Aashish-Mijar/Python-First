c9ec2708ed3313f40db33ef6096cdd607ae00ece
rep-demo <aashishmijar44@gmail.com>
2025-08-01 06:57:46 +0545

    2d file added

---
+++ 1_basics/2d.py
@@ -0,0 +1,10 @@
+matrix=[[1,3,9],[3,5,6],[6,7,2]]
+# print(matrix[0][1])
+# print(matrix[2][2])
+
+for row in matrix:
+    print(row)
+
+for row in matrix:
+    for item in row:
+        print(item)
