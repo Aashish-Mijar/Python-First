999f089e9106c36b7bdea2d4b385f9725238e1c1
rep-demo <aashishmijar44@gmail.com>
2025-08-01 06:55:57 +0545

    datatype file loaded

---
+++ 1_basics/datatype.py
@@ -0,0 +1,23 @@
+#  Integer data, no need to prefix with anything this will understand it by itsefl
+a=45
+print(a)
+
+# floating datatype
+b=3.5
+print(b)
+
+# character datatype not but in python it is considered as string of length one so no character datatype
+c='d'
+print(c)
+
+# boolean datatype
+d=False
+print(d)
+
+# string datatype
+e="Aashish"
+print(e)
+
+# none 
+f=None
+print(f)
