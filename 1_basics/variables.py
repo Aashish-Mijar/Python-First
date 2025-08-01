15dc62a2c7f391df42e092f04a60997305b43102
rep-demo <aashishmijar44@gmail.com>
2025-08-01 06:54:33 +0545

    variables file added

---
+++ 1_basics/variables.py
@@ -0,0 +1,12 @@
+# variable are containers for storing data of different datatypes
+a=1 
+b=2
+print(a+b)
+
+# Rules for variable
+# can contain alphabets, digits, and underscores.
+# can only start with an alphabet and underscores
+# can't start with a digit.
+# no white space 
+# @number = invalid due to @ symbol
+# num@ber = invalid due to @ symbol 
