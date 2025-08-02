a803383efee345938b823799aad89216ce38d05b
rep-demo <aashishmijar44@gmail.com>
2025-08-02 10:32:53 +0545

    dict file added

---
+++ 1_basics/dict.py
@@ -0,0 +1,16 @@
+dict1={
+    "name": "Aashish",
+    "rollNumber":123,
+    "address":"Bhumre"
+    }
+# print(dict1)
+# print(dict1["name"])
+for key, value in dict.items():
+    # print(info)
+    print(f"The key is {key} and value is {value}")
+    
+dict1["level"]="Bachelor"
+
+print(dict1)
+
+print()
