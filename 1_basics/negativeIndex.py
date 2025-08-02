71ae6c4a0fd23a6b40ae6c4bbed48536fd3aedcc
rep-demo <aashishmijar44@gmail.com>
2025-08-02 10:35:28 +0545

    NegativeIndex.py

---
+++ 1_basics/negativeIndex.py
@@ -0,0 +1,17 @@
+text="Programming"
+print(text[-1::-1])
+print(text[-4:-1])
+
+print(text[-1:-6:-1])
+print(text[-6:-1])
+
+# ---- Slicing from -1 to -7(-1 to -6 exclude -7) "gnimma"
+print(text[-1:-7:-1])
+
+# ---- reverse string skip every 2nd character
+print(text[-1::-2])
+
+print(text[-1::-3])
+
+print(text[7:10])
+
