b6ce07e2918e86c64e7b0ae9545090274c763f2a
rep-demo <aashishmijar44@gmail.com>
2025-08-03 19:55:46 +0545

    Tuple file added

---
+++ 1_basics/tuple.py
@@ -0,0 +1,10 @@
+person=("Ram", 25, "Waling")
+# print(person[0])
+
+# ----------Can't person these operation
+# person[0]="Hari"
+# person.append(98343)
+# person.remove(25)
+
+print(person.index("Ram"))
+print(person.count("Ram"))
