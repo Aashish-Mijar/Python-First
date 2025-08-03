b1830600b742383353c5a31ee11d133ddb4677af
rep-demo <aashishmijar44@gmail.com>
2025-08-03 06:16:22 +0545

    string file added

---
+++ 1_basics/string.py
@@ -0,0 +1,24 @@
+word="Python Programming"
+print(word)
+
+# ----Slicing String
+slicing=slice(4,6)
+print(word[slicing])
+
+# ---- reversing string
+print(word[::-1])
+
+# ----Direct method
+print(word[:4])
+
+# ----slicing after 1 letter
+print(word[::2])
+
+# ----slicing after 2 letter
+print(word[1::2])
+
+print(word[5])
+
+
+
+
