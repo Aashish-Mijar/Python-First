d9d88be2424ab0c74b6e42e9a4bd0c3e5af9a97f
rep-demo <aashishmijar44@gmail.com>
2025-08-04 07:51:41 +0545

    problem 2 file added

---
+++ 0_WarmUp/problem2.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello coders! Welcome to the world of code.")
+# engine.say("Your brain is going to hacked in 3      2      1")
+engine.runAndWait()
