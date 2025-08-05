40f40903310912b4f19bda57972e8ff7a0aa1bec
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:11:27 +0545

    function example 2 file added

---
+++ 2_ObjectOriented/Function/functionExp2.py
@@ -0,0 +1,11 @@
+def sum(a, b):
+    result=a+b
+    print("The result is:",result)
+
+def areaOfCircle(radius):
+    PI=3.1415
+    result=PI*radius*radius
+    print("The area of Circle is :", result)
+
+sum(2, 5)
+areaOfCircle(6)
