439198ad110e324aec4232f7eaad88593e4fe3ef
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:19:46 +0545

    Single Inheritance file added

---
+++ 2_ObjectOriented/Inheritance/singleInheritance.py
@@ -0,0 +1,12 @@
+class Dad:
+    def walk(self):
+        print("Dad walks differently")
+
+class Child(Dad):
+    def sing(self):
+        print("Son can sing beautifully")
+
+ch1=Child()
+ch1.walk()
+ch1.sing()
+
