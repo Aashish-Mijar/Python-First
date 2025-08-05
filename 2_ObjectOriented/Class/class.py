b98ff36b85de62db52d801c902666095f4348719
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:08:28 +0545

    class file added

---
+++ 2_ObjectOriented/Class/class.py
@@ -0,0 +1,16 @@
+class Car:
+    name=""
+    color=""
+
+    
+car1=Car()
+car2=Car()
+
+car1.name="BMW"
+car1.color="Shiny Black"
+
+car2.name="Ferrari"
+car2.color="Neon"
+
+print(f"My first car is {car1.name} and its color is {car1.color}")
+print(f"My second car is {car2.name} and its color is {car2.color}") 
