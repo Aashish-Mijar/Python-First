a484f72928b869c18083c23fef1612f4b12d8530
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:12:32 +0545

    Function 3 example file added

---
+++ 2_ObjectOriented/Function/functionExp3.py
@@ -0,0 +1,6 @@
+def stdDetail(name, rollNumber, address):
+    print("The name of the student is :",name)
+    print("The roll number of the student is :",rollNumber)
+    print("The address of the student is :",address)
+
+stdDetail("Aashish", 2, "Bhumre")
