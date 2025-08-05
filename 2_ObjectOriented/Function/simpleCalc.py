5ca2b555a2ddd6f510618c1253770d1721a079b3
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:13:24 +0545

    Simple Calculator file added

---
+++ 2_ObjectOriented/Function/simpleCalc.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a+b
+
+def subt(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
+
+addResult=add(2,3)
+print("The result of addition is:", addResult)
+
+subtResult=subt(9,3)
+print("The result of subtraction is:", subtResult)
+
+mulResult=mul(5, 4)
+print("The result of multiplication is:", mulResult)
+
+divResult=div(7, 3)
+print("The result of division is:", divResult)
