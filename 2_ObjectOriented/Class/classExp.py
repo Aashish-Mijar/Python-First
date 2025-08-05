3d2ea5462d91a2b9cbaa5a2a1bdbd165a58907a4
rep-demo <aashishmijar44@gmail.com>
2025-08-05 09:09:10 +0545

    Example 1 file added

---
+++ 2_ObjectOriented/Class/classExp.py
@@ -0,0 +1,18 @@
+class Student:
+    Fname=""
+    rollNumber=""
+    age=""
+
+std1=Student()
+std2=Student()
+
+std1.Fname="Aashish Mijar"
+std1.rollNumber=2
+std1.age=20
+
+std2.Fname="Meera S.K"
+std2.rollNumber=25
+std2.age=18
+
+print(f"Student's name is {std1.Fname} , roll number is {std1.rollNumber} and age is {std1.age}")
+print(f"Student's name is {std2.Fname} , roll number is {std2.rollNumber} and age is {std2.age}")
