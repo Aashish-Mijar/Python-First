b3d525aef30a6b7459aca84ab3760ea29e9b9765
rep-demo <aashishmijar44@gmail.com>
2025-08-06 19:28:36 +0545

    Write mode

---
+++ 3_FileHandling/expFileW.py
@@ -0,0 +1,3 @@
+file = open("example.txt", "w")
+file.write("Everythings that fades looks beautiful.\nWhen wind of changes blow some build walls and some build windmill.\nCourage is not the absence of fear but conquest of fear")
+file.close()
