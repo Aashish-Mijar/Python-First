7da14fd479e26b9a4324ccd8a8b9939e57e58d5d
rep-demo <aashishmijar44@gmail.com>
2025-08-07 07:06:25 +0545

    In slice file added

---
+++ Assignment1/in_Slice_4.py
@@ -0,0 +1,17 @@
+# ---------------- in operator
+# -------- List
+fruits=["Apple", "Cherry", "Avocado", "Mango"]
+# print("Mango" in fruits)
+
+# --------- String
+text="Welcome to Programming"
+# print("to" in text)
+
+# ---------------- Slicing
+print(fruits[2:4])
+print(fruits[-1:-3:-1])
+print(fruits[::-1])
+
+# ----------------- Indexing
+print(fruits[2])
+print(fruits[-1])
