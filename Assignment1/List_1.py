9fbaa4639e6f63d9d9a8ba59edd5fedb06f48f28
rep-demo <aashishmijar44@gmail.com>
2025-08-07 07:03:31 +0545

    List file added

---
+++ Assignment1/List_1.py
@@ -0,0 +1,24 @@
+myList=["Aashish", "Meera", "Deepsha"]
+
+
+# --- Append
+myList.append("Aayushma")
+
+# --- Insert
+myList.insert(2, "Like music")
+
+# --- sort
+myList.sort()
+
+# ---- remove()
+myList.remove("Aayushma")
+
+# ----- reverse
+myList.reverse()
+
+# ---- min
+myList2=[2, 5, 9 ,3,10]
+result1=min(myList2)
+result2=max(myList2)
+print(result2)
+# print(myList)
