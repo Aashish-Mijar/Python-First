6ca020e45954d3f58d66b19e8fa57e91cda311ab
rep-demo <aashishmijar44@gmail.com>
2025-08-07 07:12:20 +0545

    StringMan file added

---
+++ Assignment1/StringMan_9.py
@@ -0,0 +1,10 @@
+text="ProgrammingInPython"
+
+# ------ Slicing out "Programming"
+print(text[0:11])
+
+# ------ Reversing the string
+print(text[::-1])
+
+# ------ Checking if python is in the String
+print("Python" in text)
