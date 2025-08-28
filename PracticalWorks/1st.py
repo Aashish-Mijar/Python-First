9b19e794163f568f6ed79b4dc4d7c723740cd83b
rep-demo <aashishmijar44@gmail.com>
2025-08-28 21:22:10 +0545

    1st file added

---
+++ PracticalWorks/1st.py
@@ -0,0 +1,10 @@
+# Write a program to create list and add elements in list, remove elements from list and display list items.
+
+# create list
+list =[1, 4, 5, 6, 7, 8]
+
+list.append(0)  # add elements
+
+list.remove(4)  # remove elements
+
+print(list) 
