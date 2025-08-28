8e12240f30a8eb28744027f5771ba50170f9072c
rep-demo <aashishmijar44@gmail.com>
2025-08-28 21:23:59 +0545

    6th file added

---
+++ PracticalWorks/6th.py
@@ -0,0 +1,16 @@
+# Write program to create tuple, add elements in tuple, remove elements from tuple and display tuple items. 
+
+# create tuple
+tuples = (1,3,4,5)
+
+print ("The original tuple is:", tuples)
+
+# add elements in tuple
+tuples =tuples + (7,6)
+
+print ("The tuple after adding elements is:", tuples)
+
+# remove elements from tuple
+tuples = tuples[:-1]
+
+print ("The tuple after removing elements is:", tuples)
