b4f4d145167c5c9232cd93fbdd249625bd817281
rep-demo <aashishmijar44@gmail.com>
2025-08-28 21:23:35 +0545

    5th file added

---
+++ PracticalWorks/5th.py
@@ -0,0 +1,11 @@
+# Write program to illustrate two-dimensional list.
+# create a two-dimensional list (matrix)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# print the matrix
+for row in matrix:
+    print(row)
