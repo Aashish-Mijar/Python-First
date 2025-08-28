416292a4129b271f4d2cfc223450ba0a134f1f28
rep-demo <aashishmijar44@gmail.com>
2025-08-28 21:25:04 +0545

    8th file added

---
+++ PracticalWorks/8th.py
@@ -0,0 +1,16 @@
+# Write a program to create a set, add elements in set, remove elements from set and display set items. 
+
+# create a set 
+sets = set()  # this is empty sets
+
+# add elements in set
+sets.add(0)
+sets.add(1)
+sets.add(2)
+sets.add(3)
+sets.add(4)
+print("The items in a set are:",sets)
+
+# remove elements from set
+sets.remove(1)
+print("The value after the one elements from set:", sets)
