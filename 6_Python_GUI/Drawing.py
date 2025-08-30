4d89ac479bea89eea8633da1c4e2edfef4759ef9
rep-demo <aashishmijar44@gmail.com>
2025-08-30 20:58:17 +0545

    Creating text

--- 6_Python_GUI/Drawing.py
+++ 6_Python_GUI/Drawing.py
@@ -11,4 +11,10 @@ canvas.create_line(10,10,200,10, fill="blue")
 # Draw a rectangle
 canvas.create_rectangle(50, 50, 150, 100, fill="green")
 
+# Draw an oval (Circle/ellipse)
+canvas.create_oval(70, 120, 170, 170, fill="red")
 
+# Draw text
+canvas.create_text(150, 190, text="Canvas is fun!", fill="black")
+
+root.mainloop()
