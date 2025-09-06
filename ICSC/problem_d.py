1042cc47caac07ecaef2f857d81602e82a4099ff
rep-demo <aashishmijar44@gmail.com>
2025-09-06 22:17:40 +0545

    Second commit in problem d

--- ICSC/problem_d.py
+++ ICSC/problem_d.py
@@ -1,5 +1,19 @@
+# ----------- Word Search Puzzle -------
 import random
 import string
 
 # ANSI color codes
 COLOR = '\033[93m' # Yellow text
+RESET = '\033[0m'
+
+def create_crossword(words):
+    size = 10
+    grid = [['' for _ in range(size)] for _ in range(size)]
+    highlight_positions = set()
+
+    directions = [
+        (0, 1), # horizontal
+        (1, 0), # vertical
+        (1, 1), # diagonal down-right
+        (-1, 1), # diagonal up-right
+    ]
