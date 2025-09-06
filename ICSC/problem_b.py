c2d6814abd1f77ed270e130fb0e2aefb203c9fbe
rep-demo <aashishmijar44@gmail.com>
2025-09-06 15:13:05 +0545

    File changed

--- ICSC/problem_b.py
+++ ICSC/problem_b.py
@@ -1,5 +1,9 @@
-# 
+# -----Cake Calculator-----
 def cake_calculator(flour, sugar):
+
+    # Recipe requirements per cake
+    flourPerCake = 100
+    sugarPerCake = 50
     cakesFromFlour = flour // 100
     cakesFromSugar = sugar // 50
 
@@ -13,4 +17,4 @@ def cake_calculator(flour, sugar):
 # print(cake_calculator(350, 200))
 # print(cake_calculator(120, 45))
 
-print(f"The total baked cake with remaining flour and sugar respectively is ",cake_calculator(350, 200))
+print(f"The total baked cakes and remaining flour , sugar respectively is ",cake_calculator(350, 200))
